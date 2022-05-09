6ca36775dfc7a327f404bb1a93e97a1b2bb9cb6a
Daniel Marinič <daniel.marinic@itaps.sk>
2022-05-10 01:07:35 +0200

    history, database save aj get data

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ from flask_socketio import SocketIO
 from flask_cors import CORS
 import datetime as dt
 
+import mysql.connector
+#import MySQLdb.cursors
+
 import json
+from flask import jsonify
+
+import sys
 
 import eventlet
 eventlet.monkey_patch()
@@ -17,9 +23,22 @@ app.config['SERIAL_BYTESIZE'] = 8
 app.config['SERIAL_PARITY'] = 'N'
 app.config['SERIAL_STOPBITS'] = 1
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'poit'
+
 ser = Serial(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 CORS(app)
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="poit"
+)
+mycursor = mydb.cursor(buffered=True)
 
 @socketio.on('send')
 def handle_send(json_str):
@@ -30,6 +49,9 @@ def handle_send(json_str):
 @ser.on_message()
 def handle_message(msg):
     socketio.emit("serial_message", data={"message":(str(msg.decode()))})
+    data = json.loads(msg.decode())
+    #print ("""Insert Into poit (temperature, humidity) Values(%s, %s)""") %(data["temperature"],data["humidity"])
+    save_data(data["temperature"], data["humidity"])
 
 @ser.on_log()
 def handle_logging(level, info):
@@ -49,6 +71,32 @@ def test_connect(auth):
 @socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
+    
+@app.route('/data', methods=['GET'])
+def get_data_from_db():    
+    if request.method == 'GET':
+        mycursor.execute("""SELECT * FROM data""")
+        results = mycursor.fetchall()
+        return jsonify(results)
+    return jsonify([])
+        
+    #if request.method == 'POST':
+        #cursor.execute('Insert Into poit (temperature, humidity) Values( %s, %s )', (n,))
+
+def save_data(temperature, humidity):
+    try:
+        res = mycursor.execute("""Insert Into data (temperature, humidity) Values(%s, %s)""", (temperature, humidity))
+        mydb.commit()
+    except:
+        print(sys.exc_info())
+    #try:
+        #res = cursor.execute("Insert Into data (temperature, humidity) Values(%s, %s)", (temperature, humidity))
+        #mysql.connection.commit()
+    #except Exception as e:
+        #print(e)
+    #print(res)
+    #print(cursor.rowcount)
+    return "ok"
 
 if __name__ == '__main__':
     # Warning!!!
